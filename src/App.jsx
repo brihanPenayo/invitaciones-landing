7338a97846efa59e5eb6f4dc6683f14f406388e7
Brihan Penayo <brihan.penayos@gmail.com>
2023-05-09 19:34:55 -0400

    Testing TailwindCSS

--- src/App.jsx
+++ src/App.jsx
@@ -10,22 +10,22 @@ function App() {
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={viteLogo} className="font-display" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className='font-mono text-lg'>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
+        <p className='text-2xl font-body'>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className="text-3xl font-body underline">
         Click on the Vite and React logos to learn more
       </p>
     </>
