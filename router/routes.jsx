9650527e9dc46db78fd9e321bef01d1dccf5b30f
Camiroa <103272979+Camiroa@users.noreply.github.com>
2023-05-15 18:55:52 -0400

    add page to router

--- router/routes.jsx
+++ router/routes.jsx
@@ -1,14 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Contact } from "../pages/Contact";
 import App from "../src/App";
+import Carlos from "../pages/birthdays/carlos/Carlos";
 
 export const routes = createBrowserRouter([
     {
         path: "/",
-        element: <App/>
+        element: <App />
     },
     {
         path: "/test",
-        element: <Contact/>
+        element: <Contact />
+    },
+    {
+        path: "/carlos",
+        element: <Carlos />
     }
 ])
